bc0e211f848333a72f673194bcb7d709a1704137
SaptatirthaChoudhury <saptatirtha@gmail.com>
2024-05-06 21:33:04 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,33 +1,56 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
- }
+}
 
 
 export { asyncHandler }
 
 
 
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (err) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (err) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
 //_________________________Below code is only for practise purpose for above code __________________________________________
 
-// const func1 = (func2) => (val1, val2) => {  // func1 definition
- 
-//     func2(val1, val2);  // func2 called
-// }
+/* Technique 1 :_____________________
 
-// const func2 = (val1, val2) => {    // func2 definition
-//     console.log("func 2", val1, val2)
-// }
+const func1 = (func2) => (val1, val2) => { 
+      func2(val1, val2);  
+}
 
-// func1(func2(11,33))  // func1 called
+const func2 = (val1, val2) => {    
+    console.log(val1, val2)
+}
+
+func1(func2(1,2))
+
+*/
+
+
+/* Technique 2 :_________________________
+
+const func1 = function(func2){
+    const anonymous = function(val1, val2){
+        func2(val1, val2)
+    }
+}
+
+const func2 = function(val1, val2){
+    console.log(val1, val2)
+}
+
+func1(func2(1,2))
+
+*/
